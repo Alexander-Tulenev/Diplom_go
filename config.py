5e256db04e527b60786acfecb0168b4ca912217f
Alexander-Tulenev <121447502+Alexander-Tulenev@users.noreply.github.com>
2023-03-25 16:03:38 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-user_token = 'vk1.a.2PcQ5-iLd762ildQHS4bN9INLnkqZ-tFIX5CiagPZwPlhVcLTdIjdVlBaVDbnYQ6Kt28KRO-9lkj83OSlq8k19PUXDtTE04xA3kucbHnmVwi4dlP74-cGiGAo8vbm8wtZG_j5844BiCNIVeFZykkCbkqAMKxMPlWmti4EcCUkeOEwDC8it3YaHUDJbzHInp193YmCtQw0U5jk4f01d_rmQ'  # токен пользователя
-comm_token = 'vk1.a.y8Kd6f-vyWOQ-ACI998fenfsyOuyx7CXgbdXhL54XB9bo4x-piXHROWlIWEVAsABgL_TpEIjYc0zZPAv5w9PQropY1iHgeKMslIe0RvWadwsE6ypgqlGPhUjWB-wqM8y48OiMlgaT0yGCgum_9Njc4dvXoPmkvzHeaCeGpIs2b8dvsydJ-5jAVJcjxfvf8b_nQJ3ssWUAln0SOGLA60aeQ'  # токен сообщества
+user_token = ''  # токен пользователя
+comm_token = ''  # токен сообщества
 host = '127.0.0.1'
 user = 'postgres'
-password = 'Selescan731'
+password = ''
 database = 'diplom'
